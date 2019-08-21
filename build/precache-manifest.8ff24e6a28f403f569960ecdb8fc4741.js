f03832a7c564ff270f1443258534c65873fd0a17
opencert-sutd <52150238+opencert-sutd@users.noreply.github.com>
2019-08-21 14:53:11 +0800

    Add files via upload

---
+++ build/precache-manifest.8ff24e6a28f403f569960ecdb8fc4741.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "447b79fdca906c9f05f6dbe288e511fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b8d52da2d28281d0d29",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "fd5ff7246f05ce2cf6fd",
+    "url": "/static/css/main.f4d89908.chunk.css"
+  },
+  {
+    "revision": "6b8d52da2d28281d0d29",
+    "url": "/static/js/2.1efaa322.chunk.js"
+  },
+  {
+    "revision": "fd5ff7246f05ce2cf6fd",
+    "url": "/static/js/main.8527c7aa.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
